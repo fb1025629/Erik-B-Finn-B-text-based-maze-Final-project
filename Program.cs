096f621736b51e200add6d7af26d16453d18d1c7
eb1027437 <eb1027437@otc.edu>
2025-11-13 08:26:44 -0600

    start
    start of program

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Erik_B_Finn_B_text_based_maze_Final_project;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string location;
+        
+        
+    }
+}
